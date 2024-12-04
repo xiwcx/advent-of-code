9f2065e782fa0aaa45beee041a1955076dc49eee
Welch Canavan <2304131+xiwcx@users.noreply.github.com>
2024-12-04 12:30:04 -0800

    day 4, part 2

--- 2024/typescript/04/main.test.ts
+++ 2024/typescript/04/main.test.ts
@@ -1,8 +1,13 @@
 import { assertEquals } from "@std/assert";
-import { partOne } from "./main.ts";
+import { partOne, partTwo } from "./main.ts";
 import { example, myInput } from "./data.ts";
 
 Deno.test("part one", () => {
   assertEquals(partOne(example), 18);
   assertEquals(partOne(myInput), 2483);
+});
+
+Deno.test("part two", () => {
+  assertEquals(partTwo(example), 9);
+  assertEquals(partTwo(myInput), 1925);
 });
--- 2024/typescript/04/main.ts
+++ 2024/typescript/04/main.ts
@@ -63,3 +63,72 @@ export function partOne(input: string): number {
 
   return count;
 }
+
+// =====
+
+type directionPair = [direction, direction];
+const forwardSlant: directionPair = [
+  [-1, -1],
+  [1, 1],
+];
+const backwardSlant: directionPair = [
+  [-1, 1],
+  [1, -1],
+];
+
+function isSamMatch(
+  rows: string[],
+  rq: number,
+  cq: number,
+  d: directionPair,
+  x: number,
+  y: number
+): boolean {
+  const [firstX, firstY]: direction = [x + d[0][0], y + d[0][1]];
+  const [secondX, secondY]: direction = [x + d[1][0], y + d[1][1]];
+
+  if (0 > firstX || firstX >= cq || 0 > firstY || firstY >= rq) return false;
+  if (0 > secondX || secondX >= cq || 0 > secondY || secondY >= rq)
+    return false;
+
+  const firstLetter = rows[firstX][firstY];
+  const secondLetter = rows[secondX][secondY];
+
+  if (firstLetter === "S" && secondLetter === "M") return true;
+  if (firstLetter === "M" && secondLetter === "S") return true;
+
+  return false;
+}
+
+type Grid = [rows: string[], rowQ: number, colQ: number];
+
+/**
+ *
+ * https://adventofcode.com/2024/day/4#part2
+ */
+export function partTwo(input: string): number {
+  let count: number = 0;
+
+  const rows: string[] = input.split("\n");
+  const rowQuantity: number = rows.length;
+  const columnQuantity: number = rows[0].length;
+  // sugar to make calling `isSamMatch` less gnarly
+  const grid: Grid = [rows, rowQuantity, columnQuantity];
+
+  for (let row = 0; row < rowQuantity; row++) {
+    for (let column = 0; column < columnQuantity; column++) {
+      // radiate outward from any "A"
+      if (rows[row][column] === "A") {
+        // check both directions
+        if (
+          isSamMatch(...grid, forwardSlant, row, column) &&
+          isSamMatch(...grid, backwardSlant, row, column)
+        ) {
+          count++;
+        }
+      }
+    }
+  }
+
+  return count;
+}
